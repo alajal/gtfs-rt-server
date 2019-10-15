e3d64c253a091cf9888e767828809400c80a6520
alajal <anulajal@gmail.com>
2019-10-15 13:59:04 +0300

    Added post request and change in Dockerfile

--- src/server.js
+++ src/server.js
@@ -1,40 +1,31 @@
 let http = require('http');
-let url = require('url');
 let GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 
-// Create http server.
-let httpServer = http.createServer(function (req, resp) {
-    // Get client request url.
-    let reqUrlString = req.url;
-    // Get client request path name.
-    let pathName = url.parse(reqUrlString, true, false).pathname;
-    // If request login action.
-    //if ('/login' == pathName) {
-        // Get request method.
-        let method = req.method;
-        if ("POST" === method) {
-            var postData = '';
-            // Get all post data when receive data event.
-            req.on('data', function (chunk) {
-                postData += chunk;
-            });
-
-            // When all request post data has been received.
-            req.on('end', function () {
-                console.log("Client post data : " + postData);
-                let feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(postData);
-                feed.entity.forEach(function(entity) {
-                    console.log("Vehicle " + entity.vehicle.vehicle.label);
-                    if (entity.tripUpdate) {
-                        console.log("Boom " + entity.tripUpdate);
-                    }
-                });
-            })
-        }
-    //}
-});
-
-// Http server listen on port 3333.
 let port = 3333;
-httpServer.listen(port);
-console.log("Server is started.");
+http.createServer(function (request, res) {
+    //res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write('Server is started.');
+    res.end();
+    if ("POST" === request.method) {
+        let postData = '';
+        // Get all post data when receive data event.
+        request.on('data', function (chunk) {
+            postData += chunk;
+        });
+
+        // When all request post data has been received.
+        request.on('end', function () {
+            console.log("Client post data : " + postData);
+            try {
+                let feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(postData);
+                console.log();
+                feed.entity.forEach(function (entity) {
+                    console.log("Vehicle " + entity.vehicle.vehicle.label);
+                });
+            } catch (e) {
+                console.log("Something went wrong: ", e);
+            }
+
+        })
+    }
+}).listen(port);
